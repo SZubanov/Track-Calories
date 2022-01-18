017fe78901a5c21b335045f8bc2e4a09746edeb3
Sergey_Zubanov <s.zubanov@headsandhands.ru>
2022-01-18 23:54:06 +0300

    test signature oauth

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/SZubanov/Track-Calories
 
 go 1.17
-
-require github.com/dghubble/oauth1 v0.7.1 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"github.com/SZubanov/Track-Calories/fatsecret"
+)
+
 func main() {
+	fs, err := fatsecret.Connect("eec54c012a134fa3a3c1cc4f2147c579", "7d6e1365dc9741fba20afec4e61dfd65")
+	if err != nil {
+		panic(err)
+	}
+
+	foods, err := fs.GetRequestToken()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v", foods)
 }
