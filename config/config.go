fa57d7840672bccbf9d3e5b14e7250cb3c029dea
SZubanov <serge-zubanov@yandex.ru>
2022-02-02 14:56:00 +0300

    update config init

--- config/config.go
+++ config/config.go
@@ -1,29 +1,15 @@
 package config
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"github.com/go-playground/validator"
 	"github.com/spf13/viper"
+	"os"
+	"reflect"
+	"strings"
 )
 
 func init() {
-	{
-		c := Config{}
-
-		bs, err := json.Marshal(&c)
-		if err != nil {
-			panic("unable to marshal config to json")
-		}
-
-		viper.AutomaticEnv()
-		viper.SetConfigType("json")
-		if err := viper.ReadConfig(bytes.NewBuffer(bs)); err != nil {
-			panic("failed to set viper config")
-		}
-	}
-
 	viper.SetConfigType("env")
 	viper.AddConfigPath(".")
 	viper.SetConfigName(".env")
@@ -36,16 +22,16 @@ type Config struct {
 	OAuthToken        string `json:"oauth_token" mapstructure:"oauth_token" validate:"required"`
 	OAuthTokenSecret  string `json:"oauth_token_secret" mapstructure:"oauth_token_secret" validate:"required"`
 	FormUrl           string `json:"form_url" mapstructure:"form_url" validate:"required"`
-	DayInput          string `json:"dayInput" mapstructure:"day_input" validate:"required"`
-	MonthInput        string `json:"monthInput" mapstructure:"month_input" validate:"required"`
-	YearInput         string `json:"yearInput" mapstructure:"year_input" validate:"required"`
-	WeightInput       string `json:"weight" mapstructure:"weight_input" validate:"required"`
-	CaloriesInput     string `json:"calories" mapstructure:"calories_input" validate:"required"`
-	ProteinInput      string `json:"protein" mapstructure:"protein_input" validate:"required"`
-	FatInput          string `json:"fat" mapstructure:"fat_input" validate:"required"`
-	CarbohydrateInput string `json:"carbohydrate" mapstructure:"carbohydrate_input" validate:"required"`
-	FiberInput        string `json:"fiber" mapstructure:"fiber_input" validate:"required"`
-	WaterInput        string `json:"water" mapstructure:"water_input" validate:"required"`
+	DayInput          string `json:"day_input" mapstructure:"day_input" validate:"required"`
+	MonthInput        string `json:"month_input" mapstructure:"month_input" validate:"required"`
+	YearInput         string `json:"year_input" mapstructure:"year_input" validate:"required"`
+	WeightInput       string `json:"weight_input" mapstructure:"weight_input" validate:"required"`
+	CaloriesInput     string `json:"calories_input" mapstructure:"calories_input" validate:"required"`
+	ProteinInput      string `json:"protein_input" mapstructure:"protein_input" validate:"required"`
+	FatInput          string `json:"fat_input" mapstructure:"fat_input" validate:"required"`
+	CarbohydrateInput string `json:"carbohydrate_input" mapstructure:"carbohydrate_input" validate:"required"`
+	FiberInput        string `json:"fiber_input" mapstructure:"fiber_input" validate:"required"`
+	WaterInput        string `json:"water_input" mapstructure:"water_input" validate:"required"`
 }
 
 // NewConfig load current configuration.
@@ -54,6 +40,20 @@ func NewConfig() (*Config, error) {
 	if err := viper.Unmarshal(&config); err != nil {
 		return nil, fmt.Errorf("NewConfig(): failed to unmarshaled %w", err)
 	}
+
+	v := reflect.ValueOf(&config).Elem()
+	t := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		tag := t.Field(i).Tag.Get("json")
+		if val, ok := os.LookupEnv(strings.ToUpper(tag)); ok {
+			field := v.Field(i)
+			if field.CanAddr() && field.CanSet() {
+				v.Field(i).SetString(val)
+			}
+		}
+	}
+
 	err := validator.New().Struct(config)
 	if err != nil {
 		return nil, fmt.Errorf("NewConfig(): failed to validated %w", err)
